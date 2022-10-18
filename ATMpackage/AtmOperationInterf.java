2998fb03f859874ebb613f54ae0a6646bfd91c6f
Praduman-gupta <103199278+Pradumanji@users.noreply.github.com>
2022-10-18 15:01:55 +0530

    Add files via upload
    java atm project

---
+++ ATMpackage/AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package ATMpackage;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
